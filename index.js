f90414dd48c3af9dbeebb9a1246ba6d963c96d3e
LeviathanS11 <shakiulalamsas11@gmail.com>
2023-05-02 20:06:04 +0600

    vercel

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 
+const chefs=require('./data/chefs.json')
+
 app.use(cors())
+
+app.get('/chefs',(req,res)=>{
+    res.send(chefs)
+})
 
 app.get('/',(req,res)=>{
     res.send('server running')
