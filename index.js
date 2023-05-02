edfdcb07055e55d03ccfb773bfca3b7c1d3a0527
LeviathanS11 <shakiulalamsas11@gmail.com>
2023-05-02 22:58:22 +0600

    chefsDetail added

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 const chefs=require('./data/chefs.json')
+const chefDetails=require('./data/chefsDetails.json')
 
 app.use(cors())
 
 app.get('/chefs',(req,res)=>{
     res.send(chefs)
+})
+app.get('/chefs/:id',(req,res)=>{
+    const id=req.params.id;
+    const details=chefDetails.filter(n=>n.id === id);
+    res.send(details)
 })
 
 app.get('/',(req,res)=>{
